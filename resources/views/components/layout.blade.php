0bf17b84bca27327177fc67454c2c05fe8b6a787
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2024-06-07 12:30:15 -0300

    Adicionando estilos as views e adicionando bootstrap

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,11 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{$title}} | Controle de Séries</title>
+    @vite(['resources/css/app.scss','resources/js/app.js'])
 </head>
 
 <body class="">
-    <h1 class="">{{$title}}</h1>
-    {{$slot}}
+    <div class="container">
+        <h1 class="">{{$title}}</h1>
+        {{$slot}}
+    </div>
 </body>
 
 </html>
--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,6 +1,9 @@
-<x-layout title="Criar">
-    <form action="" method="post">
-        <label for="nome">Nome:</label>
-        <input type="text" name="nome" id="nome">
+<x-layout title="Nova Série">
+    <form class="" action="" method="post">
+        <div class="mb-3">
+            <label class="form-label" for="nome">Nome:</label>
+            <input class="form-control" type="text" name="nome" id="nome">
+        </div>
+        <button class="btn btn-primary" type="submit">Adicionar</button>
     </form>
 </x-layout>
