c5f5596971368d830ddd7a606e6a85b46b7c2f4b
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2024-06-18 11:27:51 -0300

    Criando relacionamento com series episodios e temporadas

--- app/Models/Serie.php
+++ app/Models/Serie.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +12,17 @@ class Serie extends Model
     use HasFactory;
 
     protected $fillable = ["nome"];
+    protected $with = ['seasons'];
+
+    public function seasons()
+    {
+        return $this->hasMany(Season::class, 'series_id');
+    }
+
+    protected static function booted()
+    {
+        self::addGlobalScope('ordered', function (Builder $queryBuilder) {
+            $queryBuilder->orderBy('nome');
+        });
+    }
 }
